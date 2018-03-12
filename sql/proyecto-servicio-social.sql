7ae66544aaa8ca9b254469f465adb1fc8c3924e1
angelgladin <angelgladin@gmail.com>
2018-03-11 22:12:16 -0600

    Esqueleto a llenar de sql

---
+++ sql/proyecto-servicio-social.sql
@@ -0,0 +1,31 @@
+/*
+ * Se creará aquí tablas, relaciones, etc de acuerdo a los casos de usos
+ * que le tocó hacer a cada integrante.
+ */
+
+/*
+ * Avistar
+ */
+-- Aquí va tu código.
+
+/*
+ * Registrar
+ */
+-- Aquí va tu código.
+
+/*
+ * Iniciar sesión
+ */-- Aquí va tu código.
+
+/*
+ * Cerrar sesión
+ */-- Aquí va tu código.
+
+/*
+ * Crear comentarios
+ */-- Aquí va tu código.
+
+/*
+ * Realizar pregunta
+ */-- Aquí va tu código.
+
